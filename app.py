5e1bf6ff0b8bae55b283c8e73427a76f61938ed9
kerusey <38439184+kerusey@users.noreply.github.com>
2021-03-24 17:41:35 +0300

    Added 404 error handler

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('Error404Page/index.html'), 404
+
+
 @app.route('/')
 def hello_world():
     return render_template('StartPage/index.html', title='welcome')
